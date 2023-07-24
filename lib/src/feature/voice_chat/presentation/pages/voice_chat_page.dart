8791c432babfb2a01dc993fcaf8fbfff761eb8c1
roothex200 <sabiturrahman60@gmail.com>
2023-07-24 16:17:04 +0600

    add loading indicator and state

--- lib/src/feature/voice_chat/presentation/pages/voice_chat_page.dart
+++ lib/src/feature/voice_chat/presentation/pages/voice_chat_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:voice_chat_bot/src/core/state/base_state.dart';
+import 'package:voice_chat_bot/src/feature/voice_chat/presentation/provider/audio_provider.dart';
 import 'package:voice_chat_bot/src/feature/voice_chat/presentation/provider/voice_button_provider.dart';
 import 'package:voice_chat_bot/src/core/utils/colors/appcolors.dart';
 import 'package:voice_chat_bot/src/feature/voice_chat/presentation/provider/voice_chat_message_list_provider.dart';
@@ -12,16 +14,18 @@ class VoiceChat extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor:Colors.pink,
-        title: const Text('Voice Chat Bot',style: TextStyle(color: Colors.white),),
+        backgroundColor: Colors.pink,
+        title: const Text(
+          'Voice Chat Bot',
+          style: TextStyle(color: Colors.white),
+        ),
         centerTitle: true,
       ),
       body: Column(
         children: [
-          
           Expanded(
             child: Consumer(
-              builder:(context,ref,child) {
+              builder: (context, ref, child) {
                 final voicechatmessage = ref.watch(voicechatmessageProvider);
                 return voicechatmessage.when(
                   loading: () => const Center(
@@ -30,47 +34,47 @@ class VoiceChat extends StatelessWidget {
                   error: (error, stackTrace) => Center(
                     child: Text(error.toString()),
                   ),
-                  data:(data) {
-                    
+                  data: (data) {
                     return ListView.separated(
                       reverse: true,
-                  padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 10),
-                  itemCount: data.length,
-                  shrinkWrap: true,
-                  primary: false,
-                  itemBuilder:(context,index) {
-                    return Row(
-                      mainAxisAlignment:data[index].me==true?MainAxisAlignment.end:MainAxisAlignment.start,
-                      children: [
-                        VoiceMessage(
-                          meBgColor: Appcolors.userrMessagecolor,
-                          contactBgColor: Appcolors.botMessagecolor,
-                          contactPlayIconBgColor: data[index].me==true?Colors.white:Colors.black,
-                          contactPlayIconColor: data[index].me==true?Colors.black:Colors.white,
-                        waveForm: const [30.0,25.0,20.0,15.0,10.0,5.0],
-                        
-                      audioSrc:
-                          data[index].voice_message_url,
-                      formatDuration: (duration) {
-                        final min = duration.inMinutes;
-                        final sec = duration.inSeconds % 60;
-                        return '$min:$sec';                      
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 10, vertical: 10),
+                      itemCount: data.length,
+                      shrinkWrap: true,
+                      primary: false,
+                      itemBuilder: (context, index) {
+                        return Row(
+                          mainAxisAlignment: data[index].me == true
+                              ? MainAxisAlignment.end
+                              : MainAxisAlignment.start,
+                          children: [
+                            VoiceMessage(
+                              meBgColor: Appcolors.userrMessagecolor,
+                              contactBgColor: Appcolors.botMessagecolor,
+                              contactPlayIconBgColor: data[index].me == true
+                                  ? Colors.white
+                                  : Colors.black,
+                              contactPlayIconColor: data[index].me == true
+                                  ? Colors.black
+                                  : Colors.white,
+
+                              played: false,
+                              audioSrc: data[index].voice_message_url,
+
+                              // To show played badge or not.
+                              me: data[index].me as bool, // Set message side.
+                              //showDuration: true,
+                              onPlay: () {}, // Do something when voice played.
+                            ),
+                          ],
+                        );
                       },
-                      
-                      played: false, // To show played badge or not.
-                      me: data[index].me as bool, // Set message side.
-                      //showDuration: true,
-                      onPlay: () {}, // Do something when voice played.
-                                ),
-                      
-                      ],
-                    ) ;
-                  }, separatorBuilder: (BuildContext context, int index) { 
-                    return const SizedBox(
-                      height: 20,
+                      separatorBuilder: (BuildContext context, int index) {
+                        return const SizedBox(
+                          height: 20,
+                        );
+                      },
                     );
-                   },
-                              );
                   },
                 );
               },
@@ -83,18 +87,21 @@ class VoiceChat extends StatelessWidget {
       ),
       floatingActionButton: Consumer(
         builder: (context, ref, child) {
+          final state = ref.watch(aduioProvider);
           final voicebutton = ref.watch(voicebuttonProvider);
           return GestureDetector(
-            onLongPressStart: (details)async {
-              print('Long Press Start');
+            onLongPressStart: (details) async {
               ref.read(voicebuttonProvider.notifier).state = true;
-            ref.read(aduioProvider.notifier).startRecordAudio();
+              ref.read(aduioProvider.notifier).startRecordAudio();
             },
             onLongPressEnd: (details) {
               ref.read(voicebuttonProvider.notifier).state = false;
               ref.read(aduioProvider.notifier).stoptRecordAudio();
             },
-            child: CircleAvatar(
+
+            child:state is LoadingState?const CircularProgressIndicator(color: Colors.pinkAccent,)
+            
+            :state is ErrorState? Text(state.data.toString(),style:const TextStyle(color: Colors.white)) :CircleAvatar(
               radius: 40,
               backgroundColor: Colors.pinkAccent,
               child: voicebutton
